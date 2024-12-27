d4901892ba0682493e6fc0d62089c34982bea5b5
Yaroslav <saturn4er@gmail.com>
2024-12-27 03:05:27 +0200

    Add default publisher to MessagesSender

--- lib/txoutbox/txoutboxwatermill/messages_sender.go
+++ lib/txoutbox/txoutboxwatermill/messages_sender.go
@@ -14,21 +14,30 @@ const OrderingKetMetadataKey = "ordering_key"
 const IdempotencyKeyMetadataKey = "idempotency_key"
 
 type MessagesSender struct {
-	topicPublishers map[string]millmessage.Publisher
-}
-
-func NewMessagesSender(topicPublishers map[string]millmessage.Publisher) *MessagesSender {
-	return &MessagesSender{
-		topicPublishers: topicPublishers,
-	}
+	topicPublishers  map[string]millmessage.Publisher
+	defaultPublisher millmessage.Publisher
 }
 
 var _ txoutbox.MessageSender = new(MessagesSender)
 
+func NewMessagesSender(
+	topicPublishers map[string]millmessage.Publisher,
+	defaultPublisher millmessage.Publisher,
+) *MessagesSender {
+	return &MessagesSender{
+		topicPublishers:  topicPublishers,
+		defaultPublisher: defaultPublisher,
+	}
+}
+
 func (m MessagesSender) SendMessage(ctx context.Context, message *txoutbox.Message) error {
 	publisher, ok := m.topicPublishers[message.Topic]
 	if !ok {
-		return fmt.Errorf("no publisher for topic %s", message.Topic)
+		if m.defaultPublisher == nil {
+			return fmt.Errorf("no publisher for topic %s", message.Topic)
+		}
+
+		publisher = m.defaultPublisher
 	}
 	watermillMessage := millmessage.NewMessage(uuid.New().String(), message.Data)
 	watermillMessage.Metadata[IdempotencyKeyMetadataKey] = message.IdempotencyKey
