0bc3002ed534c0822618a6ce19788679f4e81faf
Oleksandr V <oleksandr.vyshnevetskyi@paw-champ.com>
2025-07-03 14:03:05 +0400

    PCMD-656: add ArrayContainsAny filter

--- lib/dbutil/array_filter_expressions.go
+++ lib/dbutil/array_filter_expressions.go
@@ -38,11 +38,14 @@ func (c MappedColumnArrayFilter[T, V]) buildExpression() (clause.Expression, err
 }
 
 func ArrayFilterExpression[T, V any](value filter.ArrayFilter[T], column string, mapper func(T) (V, error)) (clause.Expression, error) {
-	if typedValue, ok := value.(*filter.ArrayContainsFilter[T]); ok {
+	switch typedValue := value.(type) {
+	case *filter.ArrayContainsFilter[T]:
 		return arrayContainsFilterGormCondition(typedValue, column, mapper)
+	case *filter.ArrayContainsAnyFilter[T]:
+		return arrayContainsAnyFilterGormCondition(typedValue, column, mapper)
+	default:
+		return nil, errors.Errorf("unsupported Filter type: %T", value)
 	}
-
-	return nil, errors.Errorf("unsupported Filter type: %T", value)
 }
 
 func arrayContainsFilterGormCondition[T, V any](containsFilter *filter.ArrayContainsFilter[T], column string, mapper func(T) (V, error)) (clause.Expression, error) { //nolint:lll
@@ -62,4 +65,21 @@ func arrayContainsFilterGormCondition[T, V any](containsFilter *filter.ArrayCont
 	}
 
 	return clause.Expr{SQL: fmt.Sprintf("%s @> ?", column), Vars: []interface{}{values}}, nil
+}
+
+func arrayContainsAnyFilterGormCondition[T, V any](containsFilter *filter.ArrayContainsAnyFilter[T], column string, mapper func(T) (V, error)) (clause.Expression, error) { //nolint:lll
+	if mapper == nil {
+		return clause.Expr{SQL: fmt.Sprintf("%s && ?", column), Vars: []interface{}{pq.Array(containsFilter.Values)}}, nil
+	}
+
+	values := make([]V, 0, len(containsFilter.Values))
+	for _, val := range containsFilter.Values {
+		mappedValue, err := mapper(val)
+		if err != nil {
+			return nil, err
+		}
+		values = append(values, mappedValue)
+	}
+
+	return clause.Expr{SQL: fmt.Sprintf("%s && ?", column), Vars: []interface{}{pq.Array(values)}}, nil
 }
