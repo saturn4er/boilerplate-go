c7994f5805dfcabfa9bbdfa8783506d0a50bd801
Oleksandr V <oleksandr.vyshnevetskyi@paw-champ.com>
2025-07-21 16:48:51 +0400

    fix empty array filter

--- lib/filter/array_is_empty.go
+++ lib/filter/array_is_empty.go
@@ -1,11 +1,9 @@
 package filter
 
-type ArrayIsEmptyFilter[E any] struct {
-	Values []E
-}
+type ArrayIsEmptyFilter[E any] struct{}
 
 func (*ArrayIsEmptyFilter[E]) isFilter([]E) {}
 
-func ArrayIsEmpty[E any](values []E) *ArrayIsEmptyFilter[E] {
-	return &ArrayIsEmptyFilter[E]{Values: values}
+func ArrayIsEmpty[E any]() *ArrayIsEmptyFilter[E] {
+	return &ArrayIsEmptyFilter[E]{}
 }
